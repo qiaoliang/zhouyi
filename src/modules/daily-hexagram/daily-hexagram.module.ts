6ad6f3cfa6a6ae621aa69e5271b0c0b8080bb311
qiaoliang <sagittatius.qiao@gmail.com>
2026-01-13 19:50:40 +0800

    fix: 修复 DailyHexagramModule 依赖注入问题
    - 添加 UserModule 和 MembershipModule 导入
    - 使 SubscriptionGuard 可以访问其依赖的 UserService
    - 解决与 DivinationModule 相同的依赖注入问题

--- src/modules/daily-hexagram/daily-hexagram.module.ts
+++ src/modules/daily-hexagram/daily-hexagram.module.ts
@@ -6,6 +6,8 @@ import { DailyHexagramService } from './daily-hexagram.service';
 import { DailyHexagramJob } from '../jobs/daily-hexagram.job';
 import { DailyHexagram, DailyHexagramSchema } from '../../database/schemas/daily-hexagram.schema';
 import { Hexagram, HexagramSchema } from '../../database/schemas/hexagram.schema';
+import { MembershipModule } from '../membership/membership.module';
+import { UserModule } from '../../services/user/user.module';
 
 /**
  * 每日一卦模块
@@ -18,6 +20,8 @@ import { Hexagram, HexagramSchema } from '../../database/schemas/hexagram.schema
       { name: Hexagram.name, schema: HexagramSchema },
     ]),
     ScheduleModule.forRoot(), // 启用定时任务
+    MembershipModule,
+    UserModule,
   ],
   controllers: [DailyHexagramController],
   providers: [DailyHexagramService, DailyHexagramJob],
